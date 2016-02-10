617f20f81a6929b9d8bd0f4cfefca11e7689854c
Tyler Steele <steeletyler@gmail.com>
2016-02-09 22:33:36 -0700

    Images and ImageButton Tutorial
    Finished the Images and ImageButton Tutorial.

--- app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
+++ app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
@@ -5,39 +5,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private PetRock rocky = new PetRock();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //setupHappyButton();
-        //setupSadButton();
-        setupEmotionButton(R.id.btnHappy, PetRock.Emotion.HAPPY);
-        setupEmotionButton(R.id.btnSad, PetRock.Emotion.SAD);
-        updateUI();
+        configureImageButton();
+
     }
 
-    private void setupEmotionButton(int buttonId, final PetRock.Emotion newEmotion) {
-        Button button = (Button) findViewById(buttonId);
-        button.setOnClickListener(new View.OnClickListener(){
+    private void configureImageButton() {
+        ImageButton btn = (ImageButton) findViewById(R.id.imageButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View arg0) {
-                rocky.setCurrentEmotion(newEmotion);
-                updateUI();
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked the button", Toast.LENGTH_LONG).show();
+
+                // Change image on button.
+                ImageButton btn = (ImageButton) findViewById(R.id.imageButton);
+                btn.setImageResource(R.drawable.x_avatar);
             }
         });
     }
 
-    private void updateUI() {
-        TextView textView = (TextView) findViewById(R.id.txtRockyFeeling);
-        String feeling = rocky.toString();
-
-        textView.setText(feeling);
-    }
 }
