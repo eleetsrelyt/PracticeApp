e137fb2bd6788a2a53b55238dd386def489ac0f2
Tyler Steele <steeletyler@gmail.com>
2016-02-10 20:56:01 -0700

    Resources by Name
    Finished the Resources by Name tutorial.

--- app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
+++ app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
@@ -6,34 +6,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private int currentDieNumber = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        configureImageButton();
-
+        setupNextImageButton();
     }
 
-    private void configureImageButton() {
-        ImageButton btn = (ImageButton) findViewById(R.id.imageButton);
-
+    private void setupNextImageButton() {
+        Button btn = (Button) findViewById(R.id.btnNextImage);
         btn.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "You clicked the button", Toast.LENGTH_LONG).show();
+                // Move to next image
+                currentDieNumber++;
+                if (currentDieNumber > 5) {
+                    currentDieNumber = 1;
+                }
 
-                // Change image on button.
-                ImageButton btn = (ImageButton) findViewById(R.id.imageButton);
-                btn.setImageResource(R.drawable.x_avatar);
+                // come up with the name of the file we want
+                String fileName = "image_" + currentDieNumber;
+
+                // translate file name to resource id
+                int id = getResources().getIdentifier(fileName, "drawable", MainActivity.this.getPackageName());
+
+                // change image based on id
+                ImageView imageView = (ImageView) findViewById(R.id.avatarImage);
+                imageView.setImageResource(id);
             }
         });
     }
+
 
 }
