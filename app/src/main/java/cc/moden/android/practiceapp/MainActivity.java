43e003b5c4223d2e3eca06d8657e6e4298917c36
Tyler Steele <steeletyler@gmail.com>
2016-02-10 21:30:39 -0700

    Worked on Context Menu Tutorial.
    I worked on the context menu tutorial, but it seems outdated. Overflow
    menu buttons have been on the way out for quite some time.

--- app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
+++ app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
@@ -1,50 +1,43 @@
 package cc.moden.android.practiceapp;
 
-import android.support.annotation.StringDef;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int currentDieNumber = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setupNextImageButton();
     }
 
-    private void setupNextImageButton() {
-        Button btn = (Button) findViewById(R.id.btnNextImage);
-        btn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                // Move to next image
-                currentDieNumber++;
-                if (currentDieNumber > 5) {
-                    currentDieNumber = 1;
-                }
-
-                // come up with the name of the file we want
-                String fileName = "image_" + currentDieNumber;
-
-                // translate file name to resource id
-                int id = getResources().getIdentifier(fileName, "drawable", MainActivity.this.getPackageName());
-
-                // change image based on id
-                ImageView imageView = (ImageView) findViewById(R.id.avatarImage);
-                imageView.setImageResource(id);
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        //super.onCreateOptionsMenu(menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
 
+        String message = "Title: " + item.getTitle()
+                + ", id=" + item.getItemId();
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+        switch(item.getItemId()) {
+            case R.id.menu_help:
+                startActivities(new Intent(this, helpScreen.class));
+                break;
+
+            case R.id.menu_settings:
+
+        }
+        return true;
+    }
 }
---
+++ app/src/main/java/cc/moden/android/practiceapp/helpScreen.java
@@ -0,0 +1,13 @@
+package cc.moden.android.practiceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class helpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_screen);
+    }
+}
