d1eb6391394010eb59238d60efe2410d81d0c642
Tyler Steele <steeletyler@gmail.com>
2016-02-14 16:06:10 -0700

    Passing Values to Activities Tutorial
    Finished the Passing values to activities using Extras tutorial.

--- app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
+++ app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
@@ -1,5 +1,7 @@
 package cc.moden.android.practiceapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ListViewCompat;
@@ -7,10 +9,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -18,85 +24,55 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private List<Car> myCars = new ArrayList<Car>();
+
+    public static final String EXTRA_USER_NUMBER = "user number";
+    public static final String EXTRA_ANSWER = "the answer";
+    public static final int RESULT_CODE_DO_MATH = 42;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        populateCarList();
-        populateListView();
-        registerClickCallback();
+        setupDoMathButton();
+
 
     }
 
+    private void setupDoMathButton() {
 
 
-
-    private void populateCarList() {
-        myCars.add(new Car("Ford", 1940, R.drawable.car184, "Needing work"));
-        myCars.add(new Car("Toyota", 1994, R.drawable.vehicle33, "Lovable"));
-        myCars.add(new Car("Honda", 1999, R.drawable.vintage40, "Wet"));
-        myCars.add(new Car("Porche", 2005, R.drawable.sedan4, "Fast!"));
-        myCars.add(new Car("Jeep", 2000, R.drawable.car80, "Awesome"));
-        myCars.add(new Car("Rust-Bucket", 2010, R.drawable.blac186, "Not *very* good"));
-        myCars.add(new Car("Moon Lander", 1971, R.drawable.vehicle46, "Out of this world"));
-    }
-
-
-    private void populateListView() {
-        ArrayAdapter<Car> adapter = new MyListAdapter();
-        ListView list = (ListView) findViewById(R.id.carsListView);
-        list.setAdapter(adapter);
-    }
-
-    private class MyListAdapter extends ArrayAdapter<Car> {
-        public MyListAdapter() {
-            super(MainActivity.this, R.layout.item_view, myCars);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            // Make sure we have a veiw to work with (may have been given null)
-            View itemView = convertView;
-            if (itemView == null) {
-                itemView = getLayoutInflater().inflate(R.layout.item_view, parent, false);
-            }
-
-            // find the car
-            Car currentCar = myCars.get(position);
-
-
-            // fill the view
-            ImageView imageView = (ImageView)itemView.findViewById(R.id.item_icon);
-            imageView.setImageResource(currentCar.getIconID());
-
-            TextView makeText = (TextView) itemView.findViewById(R.id.item_txtMake);
-            makeText.setText(currentCar.getMake());
-
-            TextView yearText = (TextView) itemView.findViewById(R.id.item_txtYear);
-            yearText.setText("" + currentCar.getYear());
-
-            TextView conditionText = (TextView) itemView.findViewById(R.id.item_txtCondition);
-            conditionText.setText(currentCar.getCondition());
-
-            return itemView;
-
-        }
-
-    }
-    private void registerClickCallback() {
-        ListView list = (ListView) findViewById(R.id.carsListView);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Button btn = (Button) findViewById(R.id.btnDoMath);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
-                Car clickedCar = myCars.get(position);
-                String message = "You clicked position: " + position + ". Which is car make " + clickedCar.getMake();
-                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+            public void onClick(View v) {
+                EditText userTextEntry = (EditText) findViewById(R.id.editTextUserNumber);
+                String userData = userTextEntry.getText().toString();
+                int userNumber = Integer.parseInt(userData);
 
+                Intent intent = new Intent(MainActivity.this, mathActivity.class);
+                intent.putExtra(EXTRA_USER_NUMBER, userNumber);
+                startActivityForResult(intent, RESULT_CODE_DO_MATH);
             }
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_CANCELED) {
+            TextView answerView = (TextView) findViewById(R.id.textViewAnswer);
+            answerView.setText("User canceled");
+            return;
+        }
+
+        switch (requestCode) {
+            case RESULT_CODE_DO_MATH:
+                int answer = data.getIntExtra(EXTRA_ANSWER, 0);
+                TextView answerView = (TextView) findViewById(R.id.textViewAnswer);
+                answerView.setText("" + answer);
+                break;
+        }
+    }
 }
