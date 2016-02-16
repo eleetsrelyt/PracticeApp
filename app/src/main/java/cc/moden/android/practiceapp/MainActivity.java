ece5fcfb7bfa9ba650c01bcfb07b69bdf937c7ef
Tyler Steele <steeletyler@gmail.com>
2016-02-15 20:52:45 -0700

    Dynamic Buttons with Images
    Finished the Dynamic Buttons with Images tutorial

--- app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
+++ app/src/main/java/cc/moden/android/practiceapp/MainActivity.java
@@ -1,78 +1,46 @@
 package cc.moden.android.practiceapp;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ListViewCompat;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_USER_NUMBER = "user number";
-    public static final String EXTRA_ANSWER = "the answer";
-    public static final int RESULT_CODE_DO_MATH = 42;
+    private static final int NUM_ROWS = 2;
+    private static final int NUM_COLS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setupDoMathButton();
-
+        populateButtons();
 
     }
 
-    private void setupDoMathButton() {
+    private void populateButtons() {
+        TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
 
+        for (int row = 0; row < NUM_ROWS; row++) {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f));
+            table.addView(tableRow);
 
-        Button btn = (Button) findViewById(R.id.btnDoMath);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText userTextEntry = (EditText) findViewById(R.id.editTextUserNumber);
-                String userData = userTextEntry.getText().toString();
-                int userNumber = Integer.parseInt(userData);
+            for (int col = 0; col <  NUM_COLS; col++) {
+                Button button = new Button(this);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f));
 
-                Intent intent = new Intent(MainActivity.this, mathActivity.class);
-                intent.putExtra(EXTRA_USER_NUMBER, userNumber);
-                startActivityForResult(intent, RESULT_CODE_DO_MATH);
+                tableRow.addView(button);
+
             }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == Activity.RESULT_CANCELED) {
-            TextView answerView = (TextView) findViewById(R.id.textViewAnswer);
-            answerView.setText("User canceled");
-            return;
-        }
-
-        switch (requestCode) {
-            case RESULT_CODE_DO_MATH:
-                int answer = data.getIntExtra(EXTRA_ANSWER, 0);
-                TextView answerView = (TextView) findViewById(R.id.textViewAnswer);
-                answerView.setText("" + answer);
-                break;
         }
     }
 }
